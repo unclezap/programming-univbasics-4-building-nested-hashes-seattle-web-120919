7a8cf8041660f72ca4d931404e9de78b08e21f3e
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 12:56:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
-        :West => "West"
+        :shortline_railroad => {}
       }
     }
   }
