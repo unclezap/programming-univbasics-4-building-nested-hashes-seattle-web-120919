e06ebc3d143842397078b981dae96d0753000461
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 12:53:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   {:railroads => {
     :pieces=> 4,
     :rent_in_dollars => {
-      :first => 100,
+      :one_piece_owned => 25,
       :second => 200,
       :third => 300,
       :fourth => 400
